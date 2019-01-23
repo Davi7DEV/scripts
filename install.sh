c835e75b1927fa4815212136e9c1813dac02bbf3
L3f7Sh <34662816+lefthacking@users.noreply.github.com>
2019-01-23 19:15:10 -0200

    Perseu-scripts

---
+++ install.sh
@@ -0,0 +1,13 @@
+echo "\033[1;31mINSTALANDO DEPENDENCIA\033[1;31m"
+echo ""
+echo "APERTE (Y) SE FOR NECESSARIO"
+
+sleep 10;
+git clone https://github.com/offensive-security/exploitdb.git
+pkg install nmap
+pkg install gcc
+pkg install lua
+chmod +x perseu.sh
+./perseu.sh
+
+echo "FINALIZADO"
